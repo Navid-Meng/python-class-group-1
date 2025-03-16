bf8e6d77dcd97da95d8aea3439ff7570ffb4bb29
Navid-Meng <navidmeng3107@gmail.com>
2025-03-16 19:05:45 +0700

    update

---
+++ exercises/exercise 1/CH1-L2-Ex1-Solution.py
@@ -0,0 +1,14 @@
+# Exercise 1
+
+name = "John"
+place_of_birth = "PP"
+current_job = "Backend dev"
+fav_num = 3107
+hours_of_sleep = 8
+fav_sport = "Volleyball"
+
+print(f"Hello! My name is {name}.")
+print(f"I'm from {place_of_birth}.")
+print(f"Right now, I'm a {current_job}.")
+print(f"I usually sleep {hours_of_sleep} hours a night.")
+print(f"My favorite sport is {fav_sport}.")
