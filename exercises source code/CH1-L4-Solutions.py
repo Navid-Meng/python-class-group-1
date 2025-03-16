3ded340498b998328c9a4f1d5333a1b3c55a2538
Navid-Meng <navidmeng3107@gmail.com>
2025-03-16 18:44:00 +0700

    updates

---
+++ exercises source code/CH1-L4-Solutions.py
@@ -0,0 +1,21 @@
+'''
+    Practice Exercises
+    Chapter 1: Python Fundamentals
+    Lesson 4: Input and Output
+'''
+
+'''
+    1. Write a program that asks the user for their 
+    favorite color and prints a message like:
+    "Your favorite color is [color]. That's awesome!"
+'''
+
+fav_color = input("Enter your favorite number: ")
+print(f"Your favorite color is {fav_color}. That's awesome.")
+
+'''
+    2. Write a program that asks the user for their full name and prints it.
+'''
+
+full_name = input("Enter your full name: ")
+print(full_name)
