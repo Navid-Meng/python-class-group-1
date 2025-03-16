d235dc46c0885ecfbc24a22a67cb35ca42bc84aa
Navid-Meng <navidmeng3107@gmail.com>
2025-03-16 18:40:55 +0700

    updates:

---
+++ exercises source code/CH1-L3-Solutions.py
@@ -0,0 +1,9 @@
+'''
+    Practice Exercises
+    Chapter 1: Python Fundamentals
+    Lesson 3: Simultaneous Assignment
+'''
+
+# Task 1: Assign the values 10, 20, and 30 to the variables a, b, and c respectively in a single line.
+
+a, b, c = 10, 20, 30
